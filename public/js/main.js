f137af226c864b186ce69c4fcbfed013da444089
Farley <farleysweb@gmail.com>
2024-06-14 17:12:36 -0400

    contact from open and close behaviour

--- public/js/main.js
+++ public/js/main.js
@@ -33,3 +33,16 @@ function handleButtonClick(e) {
     }
   })
 }
+
+document.querySelector(".form-overlay").style.display = "";
+
+function openOverlay(event) {
+  document.querySelector(".form-overlay").classList.add("form-overlay--is-visible")
+
+}
+
+document.querySelector(".close-form-overlay").addEventListener("click", closeOverlay);
+
+function closeOverlay() {
+  document.querySelector(".form-overlay").classList.remove("form-overlay--is-visible")
+}
